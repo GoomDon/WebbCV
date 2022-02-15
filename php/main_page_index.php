0aa26c88ea3486457d571e8066f2b9d5e69bd637
GoomDon <viktor.partma@gmail.com>
2022-02-15 10:35:05 +0100

    first commit

---
+++ php/main_page_index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main_page_style.css">
+    <script src="../js/main_page_main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="infoArea">
+        <div class="header">
+            <h1 id="name">Viktor Pärtma</h1>
+            <h1 class="professional title">Student på NTI Örebro</h1>
+            <h1 class="professional title">Teknikprogrammet med inriktning infromations- och medieteknik </h1>
+        </div>
+
+        <div>
+            <h1>Kontakter</h1>
+            <div class="underheadLine"></div>
+        </div>
+    </div>
+</body>
+</html>
